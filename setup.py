27f71510c2dba2be407770e3bf96e46b295b27e4
Loren Davie <loren@axilent.com>
2012-04-02 10:13:26 -0400

    Adding setup.py, moving README file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Django-Axilent',
+    version='0.1dev',
+    packages=['axilent'],
+    license='BSD',
+    long_description=open('README.md').read(),
+)
